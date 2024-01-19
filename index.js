490e872720175e3f6f2d4185689cbe557a4a1a4d
Max Schessler <schessler.max@gmail.com>
2024-01-18 18:03:25 -0600

    setting up enviornment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-app.use(express.jsoon());
+app.use(express.json());
 app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
@@ -10,4 +10,4 @@ app.post("/authenticate", async (req, res) => {
     return { username: username, secret: "sha256..." };
 }) ;
 
-app.listen(3001);
+app.listen(3001, () => console.log("Starting on port 3001"));
